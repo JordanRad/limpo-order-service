a919c0559b80bf73a1759e8e98c1dcd9f2c5e612
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-05-20 19:52:59 +0200

    .gitignore added.. Database and TestDB config added.

--- order-service/src/main/java/limpo/orderservice/controller/UserController.java
+++ order-service/src/main/java/limpo/orderservice/controller/UserController.java
@@ -1,4 +1,8 @@
 package limpo.orderservice.controller;
 
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 }
