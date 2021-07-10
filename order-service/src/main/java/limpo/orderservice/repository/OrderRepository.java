57afd2be7eabfeab43464952f99144d88bae4d57
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-07-10 16:50:23 +0200

    Minor fix

--- order-service/src/main/java/limpo/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/limpo/orderservice/repository/OrderRepository.java
@@ -17,6 +17,6 @@ public interface OrderRepository extends CrudRepository<Order,Long> {
     @Query("SELECT o FROM Order o WHERE o.orderNumber = :orderNumber")
     Optional<Order> findByOrderNumber(String orderNumber);
 
-    @Query("DELETE o FROM Order o WHERE o.orderNumber = :orderNumber")
+    @Query("DELETE FROM Order o WHERE o.orderNumber = :orderNumber")
     Optional<Order> deleteByOrderNumber(String orderNumber);
 }
